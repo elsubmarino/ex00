5d6595532b29a3446000261428624a0cd2eb1886
jiwankim <jiwankim@DESKTOP-6QDRVH1>
2017-06-03 12:25:01 +0900

    asdf

---
+++ src/test/java/org/zerock/web/MySQLConnectionTest.java
@@ -0,0 +1,19 @@
+package org.zerock.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySQLConnectionTest {
+	private static final String DRIVER="com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false&serverTimezone=UTC";
+	
+	@Test
+	public void testConnection() throws Exception{
+		Class.forName(DRIVER);
+		try(Connection con=DriverManager.getConnection(URL,"root","zxc06733")){
+			System.out.println(con);
+		}
+	}
+}
